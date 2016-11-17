3dd035cadc0f12d04caacbf42efd72be9cc461a6
YujiSoftware <yuji.software+github@gmail.com>
2016-11-18 07:57:28 +0900

    変換script、スピーカー名が取得できず途中で止まってしまう問題を修正

--- 2016-Fall/converter.js
+++ 2016-Fall/converter.js
@@ -7,17 +7,14 @@ var json = [];
 var scheduleItems = $(".schedule-item");
 for(var i = 0; i < scheduleItems.length; i++){
   var article = scheduleItems[i].getElementsByTagName("article")[0]
-  var speaker = article.getElementsByClassName("speaker-name")[0].getElementsByTagName("a")[0];
+  var speaker = article.getElementsByClassName("speaker-name")[0];
   var description = article.getElementsByClassName("description")[0];
   
   var item = {
     room: rooms[scheduleItems[i].parentNode.parentNode.id],
     time: scheduleItems[i].getElementsByClassName("time")[0].textContent,
     title: scheduleItems[i].getElementsByClassName("title")[0].textContent,
-    speaker: {
-      hash: speaker.hash,
-      name: speaker.textContent
-    },
+    speaker: speaker != null ? speaker.innerHTML : null,
     description: description.textContent
   };
   
