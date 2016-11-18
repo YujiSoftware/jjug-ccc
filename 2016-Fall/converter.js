ff0efe795ff215f7af22a2c28694833ed60f82ea
YujiSoftware <yuji.software+github@gmail.com>
2016-11-19 01:28:08 +0900

    変換script、HTML生成処理を作成

--- 2016-Fall/converter.js
+++ 2016-Fall/converter.js
@@ -3,7 +3,7 @@ for(let a of Array.from($(".tab-content .nav-schedule li a"))){
   rooms[a.hash.substring(1)] = a.textContent;
 }
 
-var json = [];
+var items = [];
 var scheduleItems = $(".schedule-item");
 for(var i = 0; i < scheduleItems.length; i++){
   var article = scheduleItems[i].getElementsByTagName("article")[0]
@@ -17,6 +17,21 @@ for(var i = 0; i < scheduleItems.length; i++){
     speaker: speaker != null ? speaker.innerHTML : null,
     description: description.textContent
   };
-  
-  console.log(item);
+
+  items.push(item);
 }
+
+var text = "";
+for(let item of items){
+  text += '<h3>' + item.time + '</h3>\r\n';
+  text += '<div>\r\n';
+  text += '  <div class="accordion session">';
+  text += '<h3><span class="room"[Room ' + item.room + ']</span><br/>' + item.title + ' <span class="hashtag">#ccc_a1</span></h3>\r\n';
+  text += '<p>' + item.speaker + '</p>\r\n';
+  text += item.description + '\r\n';
+  text += '  </div>\r\n';
+  text += '</div>\r\n';
+  text += '\r\n';
+}
+
+document.write(text);
