33aa884409e8aa7d93fc94675b42afa554396832
YujiSoftware <yuji.software+github@gmail.com>
2016-12-03 01:59:46 +0900

    発表者の詳細情報取得用スクリプトを作成

---
+++ 2016-Fall/converter_speaker.js
@@ -0,0 +1,31 @@
+var items = [];
+var speakers = $(".speaker");
+for(var i = 0; i < speakers.length; i++){
+  var item = {
+    key: speakers[i].id,
+    name: speakers[i].getElementsByClassName("name")[0].textContent,
+    img: speakers[i].getElementsByClassName("img-responsive")[0].src,
+    alt: speakers[i].getElementsByClassName("text-alt")[0].textContent,
+    about: speakers[i].getElementsByClassName("about")[0].innerHTML
+    socials:
+      Array.from(speakers[i].getElementsByClassName("speaker-socials")[0].getElementsByTagName("a")).map(a => a.href);
+  };
+
+  items.push(item);
+}
+items;
+
+var text = "";
+for(let item of items){
+  var detail = "";
+  if(item.alt != ""){
+    detail = " (" + item.alt + ")";
+  }
+  text += '<div id="' + item.key + '" class="dialog" title="' + item.name + detail + '">\r\n';
+  text += '  <p><img data-src="' + item.img + '"/></p>\r\n';
+  text += '  <p>' + item.about + '</p>\r\n';
+  text += '</div>\r\n';
+  text += '\r\n';
+}
+
+document.write(text);
