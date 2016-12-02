a61836dab8e73f4ad8bd785ad8cfb88cd28289c5
YujiSoftware <yuji.software+github@gmail.com>
2016-12-03 02:35:48 +0900

    変換scriptが動ない問題を修正

--- 2016-Fall/converter_speaker.js
+++ 2016-Fall/converter_speaker.js
@@ -6,9 +6,9 @@ for(var i = 0; i < speakers.length; i++){
     name: speakers[i].getElementsByClassName("name")[0].textContent,
     img: speakers[i].getElementsByClassName("img-responsive")[0].src,
     alt: speakers[i].getElementsByClassName("text-alt")[0].textContent,
-    about: speakers[i].getElementsByClassName("about")[0].innerHTML
+    about: speakers[i].getElementsByClassName("about")[0].innerHTML,
     socials:
-      Array.from(speakers[i].getElementsByClassName("speaker-socials")[0].getElementsByTagName("a")).map(a => a.href);
+      Array.from(speakers[i].getElementsByClassName("speaker-socials")[0].getElementsByTagName("a")).map(a => a.href)
   };
 
   items.push(item);
@@ -24,6 +24,13 @@ for(let item of items){
   text += '<div id="' + item.key + '" class="dialog" title="' + item.name + detail + '">\r\n';
   text += '  <p><img data-src="' + item.img + '"/></p>\r\n';
   text += '  <p>' + item.about + '</p>\r\n';
+  if(item.socials.length > 0){
+    text += '  <p><ul>';
+    for(let social of item.socials){
+      text += '<li><a href="' + social + '" target="_blank">' + social + '</a></li>';
+    }
+    text += '</ul></p>\r\n';
+  }
   text += '</div>\r\n';
   text += '\r\n';
 }
