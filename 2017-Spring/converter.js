b3345a7cad396900e28be69303849fdfeba2e488
YujiSoftware <yuji.software+github@gmail.com>
2017-05-08 08:06:20 +0900

    各セッションごとのハッシュタグのリンクを作成するようにした

--- 2017-Spring/converter.js
+++ 2017-Spring/converter.js
@@ -6,7 +6,8 @@ for(let a of Array.from($(".tab-content .nav-schedule li a"))){
 var items = [];
 var scheduleItems = $(".schedule-item");
 for(var i = 0; i < scheduleItems.length; i++){
-  var article = scheduleItems[i].getElementsByTagName("article")[0]
+  var article = scheduleItems[i].getElementsByTagName("article")[0];
+  var hashtag = $("a[target=blank]", article);
   var speaker = article.getElementsByClassName("speaker-name")[0];
   var description = article.getElementsByClassName("description")[0];
   
@@ -14,6 +15,7 @@ for(var i = 0; i < scheduleItems.length; i++){
     room: rooms[scheduleItems[i].parentNode.parentNode.id],
     time: scheduleItems[i].getElementsByClassName("time")[0].textContent,
     title: scheduleItems[i].getElementsByClassName("title")[0].textContent,
+    hashtag: hashtag.length > 0 ? hashtag[0].textContent : "",
     speaker: speaker != null ? speaker.innerHTML : null,
     description: description.innerHTML
   };
@@ -27,10 +29,11 @@ var text = "";
 for(let item of items){
   text += '<h3>' + item.time + '</h3>\r\n';
   text += '<div class="accordion session">\r\n';
-  text += '  <h3><span class="room">[Room ' + item.room + ']</span><br/>' + item.title + ' <span class="hashtag">#ccc_a1</span></h3>\r\n';
+  text += '  <h3><span class="room">[Room ' + item.room + ']</span><br/>' + item.title + '</h3>\r\n';
   text += '  <div>\r\n';
   text += '<p>' + item.speaker + '</p>\r\n';
   text += item.description + '\r\n';
+  text += '<p><a href="https://twitter.com/hashtag/' + item.hashtag.substring(1) + '" target="_blank">' + item.hashtag + '</a></p>\r\n';
   text += '  </div>\r\n';
   text += '</div>\r\n';
   text += '\r\n';
