1be86dc123e1746eb0b3774b00463dc637512287
YujiSoftware <yuji.software+github@gmail.com>
2017-05-09 08:00:03 +0900

    変換スクリプトで、ハッシュタグが不要な場合は出力しないようにした

--- 2017-Spring/converter.js
+++ 2017-Spring/converter.js
@@ -15,7 +15,7 @@ for(var i = 0; i < scheduleItems.length; i++){
     room: rooms[scheduleItems[i].parentNode.parentNode.id],
     time: scheduleItems[i].getElementsByClassName("time")[0].textContent,
     title: scheduleItems[i].getElementsByClassName("title")[0].textContent,
-    hashtag: hashtag.length > 0 ? hashtag[0].textContent : "",
+    hashtag: hashtag.length > 0 ? hashtag[0].textContent : null,
     speaker: speaker != null ? speaker.innerHTML : null,
     description: description.innerHTML
   };
@@ -33,7 +33,9 @@ for(let item of items){
   text += '  <div>\r\n';
   text += '<p>' + item.speaker + '</p>\r\n';
   text += item.description + '\r\n';
-  text += '<p><a href="https://twitter.com/hashtag/' + item.hashtag.substring(1) + '" target="_blank">' + item.hashtag + '</a></p>\r\n';
+  if(item.hashtag != null){
+    text += '<p><a href="https://twitter.com/hashtag/' + item.hashtag.substring(1) + '" target="_blank">' + item.hashtag + '</a></p>\r\n';
+  }
   text += '  </div>\r\n';
   text += '</div>\r\n';
   text += '\r\n';
