e6f5a55bfdf62483972dd303ddc1159bfc78e063
YujiSoftware <yuji.software+github@gmail.com>
2017-11-13 02:22:57 +0900

    JJUG CCC 2017 Fall のページを作成開始

---
+++ 2017-fall/converter.js
@@ -0,0 +1,49 @@
+var rooms = {};
+for(let a of Array.from($(".tab-content .nav-schedule li a"))){
+  rooms[a.hash.substring(1)] = a.textContent;
+}
+
+var items = [];
+var scheduleItems = $(".schedule-item");
+for(var i = 0; i < scheduleItems.length; i++){
+  var article = scheduleItems[i].getElementsByTagName("article")[0];
+  if(article != null){
+    var hashtag = $("a[target=blank]", article);
+    var speaker = article.getElementsByClassName("speaker-name")[0];
+    var description = article.getElementsByClassName("description")[0].innerHTML;
+  }else{
+    var hashtag = [];
+    var speaker = null;
+    var description = null;
+  }
+  
+  var item = {
+    room: rooms[scheduleItems[i].parentNode.parentNode.id],
+    time: scheduleItems[i].getElementsByClassName("time")[0].textContent,
+    title: scheduleItems[i].getElementsByClassName("title")[0].textContent,
+    hashtag: hashtag.length > 0 ? hashtag[0].textContent : null,
+    speaker: speaker != null ? speaker.innerHTML : null,
+    description: description
+  };
+
+  items.push(item);
+}
+
+items.sort((a,b) => a.time > b.time);
+
+var text = "";
+for(let item of items){
+  text += `<h3>${item.time}</h3>
+<div class="accordion session"${item.hashtag != null ? ' data-session="' + item.hashtag + '"' : ""}>
+  <h3><span class="room">[Room ${item.room}]</span><br/>${item.title}</h3>
+  <div>
+${item.speaker != null ? "<p>" + item.speaker + "</p>" : ""}
+${item.description}
+${item.hashtag != null ? '<p><a href="https://twitter.com/hashtag/' + item.hashtag.substring(1) + '" target="_blank">' + item.hashtag + '</a></p>' : ""}
+  </div>
+</div>
+
+`;
+}
+
+document.write(text);
